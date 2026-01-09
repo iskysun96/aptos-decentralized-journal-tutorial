241210f81dc926153c14f3d3bcc351f78c543e99
iskysun96 <iskysun96@gmail.com>
2026-01-09 11:07:12 -0800

    minor fix

--- src/view-functions/getJournalObjectAddressFromGraphQL.ts
+++ src/view-functions/getJournalObjectAddressFromGraphQL.ts
@@ -9,22 +9,6 @@ export type JournalObjectAddressResult = {
   error?: string;
 };
 
-// Helper function to safely extract address string from any format
-const extractAddressString = (value: any): string | null => {
-  // If it's already a string, return it
-  if (typeof value === 'string') {
-    console.log('value', value);
-    return value;
-  }
-  
-  // If it's wrapped in a vec array, get the first element
-  if (value?.vec && Array.isArray(value.vec) && value.vec.length > 0) {
-    return String(value.vec[0]);
-  }
-  
-  return null;
-};
-
 /*
  * Get journal object address for a user from GraphQL database
  * This queries the indexed events/data to quickly get the journal object address
